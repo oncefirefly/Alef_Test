9b295b27ba8de266defe7aeebe2e39dee83d8724
oncefirefly <oncefirefly@gmail.com>
2023-05-24 13:13:33 +0300

    added: footer component

---
+++ src/components/AlefFooter.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="footer">
+    <p class="footer__rights">all rights reserved</p>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  height: 76px;
+  background: #fafafa;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.footer__rights {
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 16px;
+  color: #000000;
+}
+</style>
